db1ee8d752ba6bc4003c0a2eef9016b623474b98
Fouls00 <ipax.fouls00@mail.ru>
2022-06-02 11:31:30 +0300

    HW

--- 3.py
+++ 3.py
@@ -1,1 +1,23 @@
+<<<<<<< Updated upstream
 
+=======
+# 3. Реализовать функцию my_func(), которая принимает три позиционных аргумента
+# и возвращает сумму наибольших двух аргументов.
+
+def my_func(arg_1, arg_2, arg_3):
+    min_arg = min(arg_1, arg_2, arg_3)
+
+    if min_arg == arg_1:
+        return arg_2 + arg_3
+    elif min_arg == arg_2:
+        return arg_1 + arg_3
+    else:
+        return arg_1 + arg_2
+
+
+# print(my_func(4, 5, 6))
+
+
+print(my_func(int(input("Print first argument:")), int(input("Print second argument:")),
+              int(input("Print third argument:"))))
+>>>>>>> Stashed changes
